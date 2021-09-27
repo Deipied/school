7f03308c10b09055f721a50d7731d2cf4a1febff
Hoon Cho <hoonhyungcho@gmail.com>
2021-09-27 12:31:26 -0500

    finalized numbersrangesum and triangular test

--- NumbersRangeSum.java
+++ NumbersRangeSum.java
@@ -1,7 +1,26 @@
+// opens the NumbersRangeSum class file
 public class NumbersRangeSum{
+    // this is the main that always runs first
     public static void main(String[] args){
-        int first = Integer.parseInt(args[0]); // assigns firstValue and converts user input of str to a double
-        int second = Integer.parseInt(args[1]);
-        
+        // establishes the commandline input of the two ints
+        int low = Integer.parseInt(args[0]); 
+        int high = Integer.parseInt(args[1]);
+        // establishes sum as a int and the getSum method
+        int sum = getSum(low, high);
+        // print statement required per instructions
+        System.out.println("The sum of natural numbers in range ["+low+","+high+"] is "+sum);
+    }
+    // this static class is the recursion part
+    public static int getSum(int lowEnd, int highEnd){
+        // it checks the two int cariables lowEnd and highEnd
+        // which start off with our input and calls upon itself recursively
+        // the first iteration through should be the else
+        // so it will return the highEnd first then add the next iteration
+        // which has 1 less than the previous iteration, until it equals the lowEnd
+        // then it exits since it is now at the lowEnd, and returns the total sum
+        if (highEnd == lowEnd)
+            return lowEnd;
+        else
+            return highEnd + getSum(lowEnd, highEnd - 1);
     }
 }
--- TriangularTest.java
+++ TriangularTest.java
@@ -1,17 +1,22 @@
-import javax.swing.plaf.TreeUI;
-
+// opens the class TriangularTest as required by java
 public class TriangularTest{
+    // the main part of the code that executes first
     public static void main(String[] args){
+        // declare 3 int inputs as a,b,c respectively 
         int a = Integer.parseInt(args[0]); 
         int b = Integer.parseInt(args[1]); 
-        int c = Integer.parseInt(args[2]); 
-        if (Triangular(a, b, c) == true)
+        int c = Integer.parseInt(args[2]);
+        // if else statement that calls the Triangular method to see if the method returns true
+        // true if its a triangle, else if not 
+        if (IsTriangular(a, b, c) == true)
             System.out.println("True"); 
         else 
             System.out.println("False");
     }
-
-    public static boolean Triangular(int a, int b, int c){
+    // a static class creation for checking the 3 inputs to see if it can make a triangle
+    public static boolean IsTriangular(int a, int b, int c){
+        // since the sum of every combination of sides must be greater than the third this is how it is set up
+        // returns true for meeting the conditions, else it is false
         if ((a + b > c) && (c + a > b) && (b + c > a))  
             return true; 
         else 
