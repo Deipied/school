e3565246b235e1b9aebab6910b26a690ad493cf6
Hoon Cho <hoon@Hoons-Air.attlocal.net>
2021-10-10 16:47:12 -0700

    finished PA 6

--- PA6/LineNum.java
+++ PA6/LineNum.java
@@ -1,30 +1,30 @@
 import java.util.*;
-
-public class LineNum{ //OPEN class
-    
-    public static void main (String[] args){ //OPEN main
-        Queue q = new Queue(50); //CREATE QUEUE CAPACITY
-        Scanner mySc = new Scanner(System.in); //DECLARES THE SCANNER OBJECT
+// this is the public class for LineNum
+public class LineNum{ 
+    // the main method
+    public static void main (String[] args){
+        Queue q = new Queue(50); //this creates a queue with a capcacity of 50
+        Scanner mySc = new Scanner(System.in); //scanner object
         
-        while(mySc.hasNext()){ //RETURNS TRUE IF SCANNER HAS ANOTHER TOKEN IN ITS INPUT
-            q.enqueue(mySc.nextLine()); //ENQUEUES STRING READ FROM SCANNER
-        } //CLOSE while LOOP
+        while(mySc.hasNext()){ //if scanner has another key in its input it will return true
+            q.enqueue(mySc.nextLine()); //reads from scanner
+        } 
         
-        int n = Integer.parseInt(args[0]); //CREATES INT VARIABLE
+        int n = Integer.parseInt(args[0]); //creates int variable n
         q.dequeue(n - 1);
-    } //CLOSE main
-} //CLOSE class
+    }
+} 
 
-// Class for queue
-class Queue{ //OPEN class QUEUE
+// creats queue class
+class Queue{ 
     
-    private String arr[]; //ARRAY TO STORE QUEUE ELEMENTS
-    private int front; //POINTS TO FRONT ELEMENT IN QUEUE
-    private int rear; //POINTS TO LAST ELEMENT IN QUEUE
-    private int capacity; //SETS MAXIMUM CAPACITY
-    private int count; //SIZE OF QUEUE
+    private String arr[]; //stores queue elemetns
+    private int front; //front element of queue
+    private int rear; //last element of queue
+    private int capacity; //sets the capacity
+    private int count; //count is the size of the queue
     
-    Queue(int size){ //CONSTRUCTOR TO INITIALIZE QUEUE
+    Queue(int size){ //queue initializer constructor
         arr = new String[size];
         capacity = size;
         front = 0;
@@ -32,40 +32,40 @@ class Queue{ //OPEN class QUEUE
         count = 0;
     }
     
-    public void dequeue(int n){ //FUNCTION TO REMOVER FRONT ELEMENT FROM QUEUE
-        if (isEmpty()){ //CHECK FOR QUEUE UNDERFLOW
-            System.out.println("UnderFlow\nProgram Terminated"); //PRINTS ERROR IF UNDERFLOW
-            System.exit(1);
-        } //CLOSE if LOOP
-        System.out.println(n + 1 + " from the first is " + arr[n]);
+    public void dequeue(int n){ //dequeue function that removes the first element of queue
+        if (isEmpty()){ //checks for underflow
+            System.out.println("UnderFlow\nProgram Terminated"); //if underflow prints the error
+            System.exit(1); //system exit
+        }
+        System.out.println(n + 1 + " from the first is " + arr[n]); //req print
         while(n < rear){ //WHILE n IS LESS THAN THE LAST ELEMENT
             arr[n]=arr[n + 1];
             n++;
         }
-        rear--; //DECREMENTS THE REAR ELEMENT
-        count--; //DECREMENTS THE SIZE
+        rear--; //reduces the last element
+        count--; //reduces the size of queue
     }
 
-    public void enqueue(String item){ //FUNCTION TO ADD ITEM TO QUEUE
-        if (isFull()){ //CHECK QUEUE FOR OVERFLOW
-            System.out.println("OverFlow\nProgram Terminated"); //PRINTS ERROR IF OVERFLOW
-            System.exit(1);
-        } //CLOSE if LOOP
+    public void enqueue(String item){ //function that adds to the queue
+        if (isFull()){ //check for queue overflow
+            System.out.println("OverFlow\nProgram Terminated"); //if overflow, print error
+            System.exit(1); //system exit
+        }
         rear = (rear + 1) % capacity;
         arr[rear] = item;
-        count++; //INCREMENTS THE SIZE
+        count++; //increases size
     }
     
-    public int size(){ //FUNCTION TO RETURN SIZE OF QUEUE
+    public int size(){ //size function just returns the count
         return count;
     }
     
-    public Boolean isEmpty(){ //FUNCTION TO CHECK IF QUEUE IS EMPTY
+    public Boolean isEmpty(){ //is Empty funcitons checks to see if queue is empty
         return (size() == 0);
     }
     
-    public Boolean isFull(){ //FUNCTION TO CHECK IF QUEUE IS FULL
+    public Boolean isFull(){ //isFull function checks to see if queue is full
         return (size() == capacity);
     }
-} //CLOSE class QUEUE
+} 
     
--- PA6/LinkedStackOfStrings.java
+++ PA6/LinkedStackOfStrings.java
@@ -1,53 +1,58 @@
-// import java.util.Scanner;
-public class LinkedStackOfStrings { // public class LinkedStackOfStrings
+import java.util.Scanner;
+
+public class LinkedStackOfStrings { // opens public class LinkedStackOfStrings
     private Node first; // private Node first
     private class Node { // private Node method
-    private String item; // private String item
-    private Node next; // private Node next
+        private String item; // private String item
+        private Node next; // private Node next
     }
-    public boolean isEmpty() { // isEmpty method
-    return first == null; // return statement
+    
+    public boolean isEmpty() { // isEmpty method that returns first == null
+        return first == null; 
     }
-    public void push(String item) { // push method
-    Node oldFirst = first; // assignment
-    first = new Node(); // created new Node first
-    first.item = item; // assignment
-    first.next = oldFirst; // assignment
+    
+    public void push(String item) { // push method onto the stack
+        Node oldFirst = first; // assigns first to oldFirst
+        first = new Node(); // created new Node first
+        first.item = item; // assigns item to first.item
+        first.next = oldFirst; // assigns oldFirst to first.next
     }
+    
     public String pop() { // pop method
-    String item = first.item; // assigns first item to item
-    first = first.next; // assigns first.next to first
-    return item; // return statement
+        String item = first.item; // assigns first item to item
+        first = first.next; // assigns first.next to first
+        return item; // returns item
     }
+    
     public boolean find(String item) { // find method
-    Node temp = first; // assigns value of first to temp Node
-    while (temp != null) { // while temp is not null
-    if(temp.item.equals(item)) return true; // if the item is in the
-   stack
-    temp = temp.next; // assignment
+        Node temp = first; // assigns first to temp
+        while (temp != null) { // while loop while temp is not null
+            if(temp.item.equals(item)) return true; // if the item is in the stack return true
+            temp = temp.next; // assigns temp.next to temp
+        }
+        return false; // return false
     }
-    return false; // return false
+    
+    public static void main(String[] args) { // the main method
+        String str = args[0]; // input assignment to str
+        LinkedStackOfStrings list = new LinkedStackOfStrings(); // list is a new linked stack of strings
+        while (!StdIn.isEmpty()) { // while loop while StdIn is empty
+            String item = StdIn.readString(); // reads strings from Std input
+            list.push(item); // pushes item into the list
+            StdOut.print(item+" "); // prints the item and a space  
+        }
+
+        if(list.find(str)) { // if the find method has the args str
+            System.out.println(str + " exists in the linked stack"); // prints str and exists in the stack
+        } else {
+            System.out.println(str + " does not exist in the linked stack"); //print not in the stack
+        }
+        public void iterate() {// iterate method
+            Node temp = first; //asssigns first to temp
+            while (temp!=null) { // while loop for temp not equaling null
+                StdOut.println(temp.item); // prints temp.item 
+                temp = temp.next; // assigns temp.next to temp
+            }
+        }
     }
-    public static void main(String[] args) { // main method
-    String str = args[0]; // string from args
-    LinkedStackOfStrings list = new LinkedStackOfStrings();
-    // list is a new linked stack of strings (stack)
-    while (!StdIn.isEmpty()) { // while Std input is empty
-    String item = StdIn.readString(); // reads strings from Std input
-    list.push(item); // puches an item into the list
-    StdOut.print(item+" "); // prints the line entry items
-   
-    }
-    if(list.find(str)) { // if find method contains the args term
-    System.out.println(str + " exists in the linked stack");
-    // prints the term exits in the stack
-    } else { // otherwise
-    System.out.println(str + " does not exist in the linked stack");
-    // java prints that the term is not in the stack
-    }
-   // You could add another method to the class to iterate (and print) the items in
-   the list.
-   // And then call it via an object of the class in the main method. The method
-   could be something like
-    // public void iterate() {
-    // Node temp = first; while (temp!=null) {
+ }
