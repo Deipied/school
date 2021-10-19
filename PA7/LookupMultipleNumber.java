a19d87114dfb9be17d5a20cfc4f6866f263ba002
Hoon Cho <hoonhyungcho@gmail.com>
2021-10-19 10:08:34 -0500

    initialized final project

--- PA7/LookupMultipleNumber.java
+++ PA7/LookupMultipleNumber.java
@@ -1,78 +1,93 @@
-public class LookupMultipleNumber{ //opens public class LookupMultipleNumber    
-    public static void main(String[] args){ //the main method
-        In in = new In(args[0]); //assigns in to read from input
-        int n = Integer.parseInt(args[1]); //CLI n which is int
-        int keyField = Integer.parseInt(args[2]); //CLI kekField second arg
-        int valField = Integer.parseInt(args[3]); //CLI valField third arg
-        String[] database = in.readAllLines(); //reads from input file as string and stores in database
-        StdRandom.shuffle(database); //shuffles the database 
-        ST<String, Queue<String>> st = new ST<String, Queue<String>>(); //creating the symbol table
-        for (int i = 0; i < database.length; i++){ //for loop that iterates over the database
-            String[] tokens = database[i].split(","); // stores database tokens in the token array
-            String key = tokens[keyField]; // assigns key from the tokens array
-            String val = tokens[valField]; // assigns val from the tokens array
-            
-            if (st.get(key) == null){ // if key not in table
-                Queue<String> que = new Queue<String>(); //create a queue
-                que.enqueue(val); // adds val to queue
-                st.put(key, que); //store they key and queue in the table
-            } else {
-            Queue<String> que = st.get(key); //finds the queue with the key 
-            que.enqueue(val); // adds key to the queue
-            st.put(key,que); // key queue pair into the table
-            }
-        } 
-        while (!StdIn.isEmpty()){ //while loop for while input is empty
-            String s = StdIn.readString(); //assigns s from input string
-            if (st.contains(s)){ //checks to see it contains s
-                Queue<String> queue = st.get(s); // get and assign to queue to add to table
-                for(int i = 0; i < n && !queue.isEmpty(); i++){ // for loop to iterate 
-                    StdOut.print(queue.dequeue() + " ");//prints the queue and space
-                }
+public class LookupMultipleNumber { //OPEN CLASS
+    public static void main(String[] args) { //OPEN MAIN
+        In in = new In(args[0]); //CREATE AN In OBJECT FOR READING DATA FROM INPUT FILE
+        //COMMAND LINE ARGUMENT INPUTS, FIRST NUMERIC VALUE INTO n, SECOND INTO keyField, THIRD INTO valField
+        int n = Integer.parseInt(args[1]);
+        int keyField = Integer.parseInt(args[2]);
+        int valField = Integer.parseInt(args[3]);
+        //READ LINES FROM INPUT FILE AS STRINGS, STORE THEM IN THE STRING ARRAY database
+        String[] database = in.readAllLines();
+        StdRandom.shuffle(database);
+        ST<String, Queue<String>> st = new ST<String, Queue<String>>();
+        //CREATES SYMBOL TABLE
+        for (int i = 0; i < database.length; i++) { //ITERATE OVER ALL LINES STORED IN THE DATABASE
+            String[] tokens = database[i].split(","); //STORE EACH LINE INTO TOKENS, STORE THEM IN STRING ARRAY tokens
+            String key = tokens[keyField]; //THE keyField INDEX IN TOKENS ARRAY STORES THE KEY. GETS ASSIGNED TO STRING VARIABLE key
+            String val = tokens[valField]; //THE valField INDEX IN TOKENS ARRAY STORES THE VALUE. GETS ASSIGNED TO STRING VARIABLE val
+            //IF THE KEY IS NOT IN SYMBOL TABLE, CREATE A QUEUE, ADD THE val TO QUEUE,
+            //STORE THE KEY,QUEUE PAIR IN THE SYMBOL TABLE
+            if (st.get(key) == null) {
+                Queue<String> que = new Queue<String>();
+                que.enqueue(val);
+                st.put(key, que);
+            } //CLOSE if
+            else { //OR ELSE GETS THE QUEUE ASSOCIATED WITH THE KEY, ADDS THE KEY TO IT AND PUT THE KEY,QUEUE PAIR IN THE SYMBOL TABLE
+                Queue<String> que = st.get(key);
+                que.enqueue(val);
+                st.put(key,que);
+            } //CLOSE else
+        } //CLOSE for
+        //WHILE SETUP TO READ VALUES ENTERED BY THE USER
+        while (!StdIn.isEmpty()) { //READ KEY AND PROVIDE VALUE
+            String s = StdIn.readString();
+            if (st.contains(s)) {
+                Queue<String> queue = st.get(s);
+                for(int i = 0; i < n && !queue.isEmpty(); i++) {
+                    StdOut.print(queue.dequeue() + " ");
+                }//CLOSE for
                 StdOut.println();
-            } else {
-                StdOut.println("Not found"); //if not found
-            }            
-        } 
-    } 
-} 
+            } //CLOSE if
+            else {
+                StdOut.println("Not found");
+            }
+        } //CLOSE while
+    } //CLOSE MAIN
+} //CLOSE CLASS
 
-class Queue<Item>{ //oepsn queue class
-    private Node first; //priavte first
-    private Node last; // pricate last
-    private class Node { //opens node class
-        private Item item;// private item
-        private Node next; // private next
-    } 
-    public boolean isEmpty() { //isEmpty method
+
+class Queue<Item> { //OPEN Queue CLASS
+    private Node first;
+    private Node last;
+    
+    private class Node { //OPEN Node CLASS
+        private Item item;
+        private Node next;
+    } //CLOSE Node CLASS
+
+    public boolean isEmpty() { //OPEN isEmpty
         return (first == null);
-    }
-    public void enqueue(Item item) { //adds to end of queue
-        Node oldLast = last; //assigns last to oldLAst
-        last = new Node(); // assigns new Node to last
-        last.item = item; // assign item to last.item
-        last.next = null; // asigns null to last.next
-        if (isEmpty()){ //if empty
-            first = last; //assign last to first
+    } //CLOSE isEmpty
+
+    public void enqueue(Item item) { //INSERT NEW NODE AT END OF LIST
+        Node oldLast = last;
+        last = new Node();
+        last.item = item;
+        last.next = null;
+        if (isEmpty()) {
+            first = last;
         } else {
-            oldLast.next = last; // else assign last to oldLast.next
+            oldLast.next = last;
         }
-    }
-    
-    public Item dequeue() { //dequeue method
-        Item item = first.item;//assigns first.item to item
-        first = first.next; //assigns first.next to first
-        if (isEmpty()) last = null; // if empty assign null to last
-        return item;// returns item
-    }
-    
-    public String toString(){ //to string method
-        Node current = first; //assigns first to current
-        String output = (String)first.item; // assigns first.item to output
-        while (current.next != null){ //while loop while curren.tnext does not equal null
-            current = current.next; //assigns curent.next to current
-            output.concat("," + current.item); //adds current.item to output
+    } //CLOSE enqueue
+
+    public Item dequeue() { //REMOVE THE FIRST NODE FROM THE LIST AND RETURN ITEM
+        Item item = first.item;
+        first = first.next;
+        if (isEmpty()) {
+            last = null;
         }
+        return item;
+    } //CLOSE dequeue
+    
+    //ITERATE THROUGH EACH NODE IN QUEUE AND APPEND THEIR ITEM VALUES String TO RETURN
+    @Override public String toString(){
+        Node current = first;
+        String output = (String)first.item;
+        
+        while (current.next != null){
+            current = current.next;
+            output.concat("," + current.item);
+        } //CLOSE while
         return output;
-    } 
-} 
+    } //CLOSE String
+} //CLOSE Queue CLASS
