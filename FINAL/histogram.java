e07e67e089c3142d53f625fcd637d4d0a6c0a916
Hoon Cho <hoonhyungcho@gmail.com>
2021-10-20 09:16:48 -0500

    made progress on final project histogram.java

--- FINAL/histogram.java
+++ FINAL/histogram.java
@@ -1,65 +1,42 @@
 public class Histogram {
     public static void main(String[] args){
-        //Create an integer array data from the text file specified via redirect operator
-        int[] data = StdIn.readAllInts();
+        int[] data = StdIn.readAllInts();// data array creation from input
+        int[] x = new int[n+1]; //assigns x to hold x axis points
+        int n = data[0]; // assigns n to first data point, which is # of intervals
+        int left = data[1]; // assigns left to the second value for the left end of interval
+        int right = data[2];// assigns right for the second value, right end of interval
+        int interval = (right - left)/n; //assigns # of intervals to interval
+        double[] amount = new double[n]; // double array assignment to amount with size of n, double is req for plot
+        double dimensions = 0; //declare dimensions that holds the dimensions based on the histogram
 
-        //Assign the first integer to n for the number of intervals
-        int n = data[0];
-
-        //Assign the second integer to left for the left end of the interval
-        int left = data[1];
-
-        //Assign the third integer to right for the right end of the interval
-        int right = data[2];
-
-        //Create a double (since the plotBars method takes a double array) array
-        //of size n called freq that contains the number of values in each interval
-        double[] freq = new double[n];
-
-        //Compute the number of intervals and assign it to integer variable interval
-        int interval = (right - left)/n;
-
-        //Create an integer array xaxis of size n+1 that would hold the x axis interval points
-        int[] xaxis = new int[n+1];
-
-        //Populate the xaxis array with x axis interval points
-        for (int i = 0; i < xaxis.length; i++) {
-            xaxis[i] = left + i*interval;
+        for (int i = 0; i < x.length; i++) { //for loop for x array that puts in values for x axis
+            x[i] = left + i*interval;
         }
 
-        //declare and initialize a double variable max to be used for specifying the
-        //window dimensions that would display the histogram
-        double max = 0;
-
-        //loop for computing the frequency for each inteval and storing it in freq array
-        for (int i = 0; i < xaxis.length-1; i++) {
-        //Declare and initialize count to 0
-        int count = 0;
+        for (int i = 0; i < x.length-1; i++) { //for loop that calc the amount and stores in array
+            int count = 0; // declare count equal to 0 
 
             for (int j = 0; j < data.length-3; j++) {
             // Checks each value in the text file to see if it falls within each interval.
-                if (data[j+3] >= xaxis[i] && data[j+3] < xaxis[i+1]) {
+                if (data[j+3] >= x[i] && data[j+3] < x[i+1]) {
                 //If value falls withing the current interval, increment count by one
                 count++;
                 }
             }
 
-            //Set freq[i] to count, or number of values within each interval
-            freq[i] = count;
+            //Set amount[i] to count, or number of values within each interval
+            amount[i] = count;
 
-            //Set max to freq[i] if its greater than it.
-            if (freq[i] > max){ 
-                max = freq[i];
+            //Set dimensions to amount[i] if its greater than it.
+            if (amount[i] > dimensions){ 
+                dimensions = amount[i];
             }
-        } //end of loop
+        } 
 
         //Set canvas size to 500 by 500
         StdDraw.setCanvasSize(500, 500);
-
-        //Set y scale from -1 to max + 1, to leave a little border on both ends
-        StdDraw.setYscale(-1, max + 1);
-
-        //Display the histogram using standard method
-        StdStats.plotBars(freq);
+        //Set y scale from -1 to dimensions + 1, to leave a little border on both ends
+        StdDraw.setYscale(-1, dimensions + 1);
+        StdStats.plotBars(amount); //the method for displaying
     }
 }
